46a764b17611aae637db55a9daeb64bb6e451477
Ryan <rburtson@udel.edu>
2024-11-05 01:16:51 -0500

    Added all Controllers

---
+++ WebRest/Controllers/OrderStatusController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.EntityFrameworkCore;
+using WebRestEF.EF.Data;
+using WebRestEF.EF.Models;
+using WebRest.Interfaces;
+namespace WebRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderStatusController : ControllerBase, iController<OrderStatus>
+    {
+        private readonly WebRestOracleContext _context;
+
+        public OrderStatusController(WebRestOracleContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/OrderStatuses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderStatus>>> Get()
+        {
+            return await _context.OrderStatuses.ToListAsync();
+        }
+
+        // GET: api/OrderStatuses/5
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<OrderStatus>> Get(string id)
+        {
+            var orderStatus = await _context.OrderStatuses.FindAsync(id);
+
+            if (orderStatus == null)
+            {
+                return NotFound();
+            }
+
+            return orderStatus;
+        }
+
+        // PUT: api/OrderStatuses/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, OrderStatus orderStatus)
+        {
+            if (id != orderStatus.OrderStatusId)
+            {
+                return BadRequest();
+            }
+            _context.OrderStatuses.Update(orderStatus);
+
+
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderStatusExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/OrderStatuses
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<OrderStatus>> Post(OrderStatus orderStatus)
+        {
+            _context.OrderStatuses.Add(orderStatus);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrderStatus", new { id = orderStatus.OrderStatusId }, orderStatus);
+        }
+
+        // DELETE: api/OrderStatuses/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var orderStatus = await _context.OrderStatuses.FindAsync(id);
+            if (orderStatus == null)
+            {
+                return NotFound();
+            }
+
+            _context.OrderStatuses.Remove(orderStatus);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrderStatusExists(string id)
+        {
+            return _context.OrderStatuses.Any(e => e.OrderStatusId == id);
+        }
+    }
+}
